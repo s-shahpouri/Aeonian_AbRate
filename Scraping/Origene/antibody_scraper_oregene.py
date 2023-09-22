e6117c6a9ae4ed04002e67c6c352f94a97b1aa7e
s-shahpouri <z.shahpouri@gmail.com>
2023-09-22 08:34:29 +0200

    Using Beautiful soup, name and url are scrapped.

--- Scraping/Origene/antibody_scraper_oregene.py
+++ Scraping/Origene/antibody_scraper_oregene.py
@@ -1,36 +1,87 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+import requests
+import re
+from bs4 import BeautifulSoup
 
 # Define the base URL and search query
 base_url = "https://www.origene.com"
 search_query = "AKT3"
 
-# Initialize a Selenium webdriver (make sure to download and configure the Chrome webdriver)
-driver = webdriver.Chrome()
+# Create a session to handle cookies (optional)
+session = requests.Session()
 
-# Navigate to the search URL
+# Set the User-Agent header (optional)
+headers = {
+    "User-Agent": "Your_User_Agent_String_Here"
+}
+
+# Perform a search for "AKT3"
 search_url = f"{base_url}/search?category=Antibodies&q={search_query}"
-driver.get(search_url)
 
-# Wait for the product links to be present (adjust the timeout as needed)
-wait = WebDriverWait(driver, 10)
-product_links = wait.until(
-    EC.presence_of_all_elements_located((By.CLASS_NAME, "name")))
+try:
+    response = session.get(search_url, headers=headers)
+    response.raise_for_status()  # Raise an exception for 4xx and 5xx status codes
 
-# Extract product names and URLs using JavaScript execution
-products = []
-for link in product_links:
-    product_name = driver.execute_script(
-        "return arguments[0].textContent;", link)
-    product_url = link.get_attribute("href")
-    products.append({"name": product_name.strip(), "url": product_url})
+    soup = BeautifulSoup(response.text, "html.parser")
 
-# Print the scraped product names and URLs
-for product in products:
-    print(f"Product Name: {product['name']}")
-    print(f"Product URL: {product['url']}")
+    # Find all product links with class "name"
+    product_links = soup.find_all("a", class_="name")
 
-# Close the Selenium webdriver
-driver.quit()
+    # Initialize a list to store product information
+    products_info = []
+
+    for link in product_links:
+        product_name = link.text.strip()
+        product_url = base_url + link["href"]
+
+        # Visit the product page and scrape additional information
+        product_response = session.get(product_url, headers=headers)
+        product_soup = BeautifulSoup(product_response.text, "html.parser")
+
+        # Extract CAT# using regex
+        cat_match = re.search(
+            r'<h2 class="sku mt-0">CAT#: (.+?)</h2>', str(product_soup))
+        cat_num = cat_match.group(1) if cat_match else 'N/A'
+
+        # Extract long description using regex
+        description_match = re.search(
+            r'<p class="long-description mt-3">(.+?)</p>', str(product_soup))
+        product_description = description_match.group(
+            1).strip() if description_match else 'N/A'
+
+        # Extract available and sold-out sizes using regex
+        size_match = re.findall(
+            r'(<a class="btn btn-sm btn-product-options" href=".+?">(.+?)</a>)|(<span class="btn btn-sm btn-product-options disabled">(.+?)</span>)', str(product_soup))
+
+        available_sizes = [s[1] for s in size_match if s[1]]
+        sold_out_sizes = [s[3] for s in size_match if s[3]]
+
+        # Create a dictionary to store all information for this product
+        product_info = {
+            "name": product_name,
+            "url": product_url,
+            "cat_num": cat_num,
+            "description": product_description,
+            "available_sizes": available_sizes,
+            "sold_out_sizes": sold_out_sizes
+        }
+
+        # Append the product information to the list
+        products_info.append(product_info)
+
+    # Print or process the scraped product information
+    for product in products_info:
+        print(f"Product Name: {product['name']}")
+        print(f"Product URL: {product['url']}")
+        print(f"CAT#: {product['cat_num']}")
+        print(f"Product Description: {product['description']}")
+        print(
+            f"Available Sizes: {', '.join(product['available_sizes']) if product['available_sizes'] else 'N/A'}")
+        print(
+            f"Sold Out Sizes: {', '.join(product['sold_out_sizes']) if product['sold_out_sizes'] else 'N/A'}")
+        print("-----")
+
+except requests.exceptions.RequestException as e:
+    print(f"An error occurred while making the request: {e}")
+
+except Exception as e:
+    print(f"An error occurred: {e}")
