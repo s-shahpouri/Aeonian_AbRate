9e8044cba159fc34f812b0a03bc7125dea44bdaf
s-shahpouri <z.shahpouri@gmail.com>
2023-09-22 08:36:45 +0200

    Extracting more information about the product.

--- Scraping/Origene/antibody_scraper_oregene.py
+++ Scraping/Origene/antibody_scraper_oregene.py
@@ -35,49 +35,56 @@ try:
 
         # Visit the product page and scrape additional information
         product_response = session.get(product_url, headers=headers)
+        product_page_html = product_response.text
         product_soup = BeautifulSoup(product_response.text, "html.parser")
 
-        # Extract CAT# using regex
-        cat_match = re.search(
-            r'<h2 class="sku mt-0">CAT#: (.+?)</h2>', str(product_soup))
-        cat_num = cat_match.group(1) if cat_match else 'N/A'
+        # Extract the necessary fields using regex
+        sku_search = re.search(r'CAT#:\s*(\w+)', product_page_html)
+        sku = sku_search.group(1) if sku_search else 'N/A'
 
-        # Extract long description using regex
-        description_match = re.search(
-            r'<p class="long-description mt-3">(.+?)</p>', str(product_soup))
-        product_description = description_match.group(
-            1).strip() if description_match else 'N/A'
+        long_description_search = re.search(
+            r'<p class="long-description mt-3">(.*?)</p>', product_page_html, re.DOTALL)
+        long_description = long_description_search.group(
+            1).strip() if long_description_search else 'N/A'
 
-        # Extract available and sold-out sizes using regex
-        size_match = re.findall(
-            r'(<a class="btn btn-sm btn-product-options" href=".+?">(.+?)</a>)|(<span class="btn btn-sm btn-product-options disabled">(.+?)</span>)', str(product_soup))
+        sizes = re.findall(
+            r'<a class="btn btn-sm btn-product-options".*?>(.*?)</a>|<span class="btn btn-sm btn-product-options disabled">(.*?)</span>', product_page_html)
 
-        available_sizes = [s[1] for s in size_match if s[1]]
-        sold_out_sizes = [s[3] for s in size_match if s[3]]
+        sizes = [size[0] or size[1] for size in sizes]
+        formulation = re.findall(
+            r'<p class="smaller text-muted mb-1">.*?Formulation:.*?<span class="btn btn-sm btn-product-options.*?">(.*?)</span>', product_page_html, re.DOTALL)
 
-        # Create a dictionary to store all information for this product
+        conjugation = re.findall(
+            r'<p class="smaller text-muted mb-1">.*?Conjugation:.*?<span class="btn btn-sm btn-product-options.*?">(.*?)</span>', product_page_html, re.DOTALL)
+
+        special_offer_search = re.search(
+            r'<p>\s*<a class="origene-orange".*?>(.*?)</a>\s*</p>', product_page_html, re.DOTALL)
+        special_offer = special_offer_search.group(
+            1).strip() if special_offer_search else 'N/A'
+
         product_info = {
             "name": product_name,
             "url": product_url,
-            "cat_num": cat_num,
-            "description": product_description,
-            "available_sizes": available_sizes,
-            "sold_out_sizes": sold_out_sizes
+            "sku": sku,
+            "long_description": long_description,
+            "sizes": sizes,
+            "formulation": formulation,
+            "conjugation": conjugation,
+            "special_offer": special_offer
         }
 
-        # Append the product information to the list
         products_info.append(product_info)
 
     # Print or process the scraped product information
     for product in products_info:
         print(f"Product Name: {product['name']}")
         print(f"Product URL: {product['url']}")
-        print(f"CAT#: {product['cat_num']}")
-        print(f"Product Description: {product['description']}")
-        print(
-            f"Available Sizes: {', '.join(product['available_sizes']) if product['available_sizes'] else 'N/A'}")
-        print(
-            f"Sold Out Sizes: {', '.join(product['sold_out_sizes']) if product['sold_out_sizes'] else 'N/A'}")
+        print(f"SKU: {product['sku']}")
+        print(f"Long Description: {product['long_description']}")
+        print(f"Sizes: {', '.join(product['sizes'])}")
+        print(f"Formulation: {', '.join(product['formulation'])}")
+        print(f"Conjugation: {', '.join(product['conjugation'])}")
+        print(f"Special Offer: {product['special_offer']}")
         print("-----")
 
 except requests.exceptions.RequestException as e:
