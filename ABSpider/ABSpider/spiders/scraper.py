261651daa8c1ff6f9525b354d57e37a06929912a
s-shahpouri <z.shahpouri@gmail.com>
2023-09-24 18:36:42 +0200

    Correcting name of varaibles.

--- ABSpider/ABSpider/spiders/scraper.py
+++ ABSpider/ABSpider/spiders/scraper.py
@@ -1,19 +1,19 @@
 import scrapy
 
-class BookspiderSpider(scrapy.Spider):
+class ABspiderSpider(scrapy.Spider):
     name = 'scraper'
     allowed_domains = ['abcam.com']
     start_urls = ['https://www.abcam.com/en-nl/products/primary-antibodies']
     main = 'https://www.abcam.com'
     def parse(self, response):
         # <div class="pb-4 mb-4 border-b border-gray"><p class="font-bold text-gray-400 text-body-small">ab1791</p><p class="font-bold"><a href="/en-nl/products/primary-antibodies/anti-histone-h3-antibody-nuclear-marker-and-chip-grade-ab1791">Anti-Histone H3 antibody - Nuclear Marker and ChIP Grade</a></p><div class="flex pt-2 gap-2 flex-wrap"><div class="appearance-none px-2 py-1 rounded-4px bg-blue-5 text-xs font-semibold text-blue-default tracking-wider" data-testid="tag">Advanced Validation</div></div></div>
-        books = response.css('div.pb-4')
+        products = response.css('div.pb-4')
 
-        for book in books:
+        for product in products:
             yield{
-                'name' : book.css('a::text').get(),
-                'id' : book.css('p.font-bold::text').get(),
-                'url' : book.css('a').attrib['href'],
+                'name' : product.css('a::text').get(),
+                'id' : product.css('p.font-bold::text').get(),
+                'url' : product.css('a').attrib['href'],
             }
 
         next_page = response.css('a[aria-label="Next page"]::attr(href)').get()
